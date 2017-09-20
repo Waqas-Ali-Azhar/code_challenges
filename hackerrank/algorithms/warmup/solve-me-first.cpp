c12efa13f22aba68ba81f4518ff011302c0859a1
asdads <waqas_111@hotmail.com>
2017-09-21 00:01:47 +0500

    solve me first

---
+++ hackerrank/algorithms/warmup/solve-me-first.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int solveMeFirst(int a, int b) {
+    int s1;
+    s1=a+b;
+    return s1;
+}
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
