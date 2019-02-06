47fd0e70710dbe7cfa6b7cb09590767600628245
Waqas Ali <waqas_111@hotmail.com>
2019-02-06 23:24:27 +0500

    Add two numbers

--- leetcode/Add-Two-Numbers.cpp
+++ leetcode/Add-Two-Numbers.cpp
@@ -8,56 +8,52 @@
  */
 class Solution {
 public:
-    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* p1 = l1;
-        ListNode* p2 = l2;
-        ListNode* next;
-        int sum;
-        
-        
-        while(p1->next!=NULL && p2->next!=NULL){
-            next = p1->next;
-            sum = p1->val + p2->val;
-            if(sum<10){
-               p1->val = sum;
-               p1 = next;
-               next = next->next;
-               p2 = p2->next; 
-            }
-            else if(sum>=10){
-                
-                next->val = (next->val + sum/10); 
-                p1->val = sum%10;
-                p1 = next;
-                next = next->next;
-                p2 = p2->next; 
-                
-            }
-
-         sum = p1->val+p2->val;
-         
-        
+        if(!l1){
+            return l2;
         }
+        if(!l2){
+            return l1;
+        }
+        if(!l1 && !l2)
+            return NULL;
+        ListNode* ptr1 = l1;
+        ListNode* ptr2 = l2;
         
-    if(sum>10){
-            ListNode* ptr= p1;
-            
-            if(p2->next!=NULL){
-              p2->next->val = sum/10;
-              p1->val = sum % 10;
+        ListNode* newList = new ListNode(0);
+        ListNode* ptrNewList = newList;
+        int carry =0;
+        while(ptr1!= NULL || ptr2!= NULL){
+            int value = 0;
+            if(ptr1!=NULL && ptr2!=NULL){
+                value = ptr1->val + ptr2->val + carry;
             }
-            else{
-                ptr->val =  sum /10;  
-                p1->val = sum % 10;
-                ptr->next = p1->next;
-                p1->next = ptr;  
+            else if(ptr2 != NULL){
+                value = ptr2->val + carry;
+            }
+            else if(ptr1!=NULL){
+                value = ptr1->val + carry;
             }
             
+            newList->val =  value % 10;
+            carry = value / 10;
+            
+            
+            if(ptr1!=NULL)
+                ptr1 = ptr1->next;
+            if(ptr2!=NULL)
+                ptr2 = ptr2->next;
+            if(ptr1!=NULL || ptr2!=NULL || carry>0){
+                newList->next =new  ListNode(0);
+                newList = newList->next;    
+            }
             
         }
         
+        if(carry>0){
+            newList->val = carry;
+        }
         
-        return l1;
+        return ptrNewList;
     }
 };
