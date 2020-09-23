effa336d49a28ae64aa5213445a9fbade066b143
Waqas Ali <waqas_111@hotmail.com>
2020-09-23 13:28:11 +0500

    symmetric tree completed

--- leetcode/symmetric-tree.py
+++ leetcode/symmetric-tree.py
@@ -1,5 +1,4 @@
 # Definition for a binary tree node.
-#https://leetcode.com/problems/symmetric-tree/
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -7,12 +6,12 @@
 #         self.right = right
 class Solution:
     flag = 0;
-    pRoots = []
-    qRoots = []
+    
     
     def checkSame(self, p: TreeNode, q: TreeNode):
             if(p.left is not None and q.right is not None):
                 return self.checkSame(p.left, q.right)
+            
             if(p.left is None):
                 self.flag = 1
             if(q.right is None):
@@ -26,6 +25,7 @@ class Solution:
             
             if(p.right is not None and q.left is not None):
                 return self.checkSame(p.right , q.left)
+            
             if(p.right is None):
                 self.flag = 1
             if(p.left is None):
@@ -39,41 +39,44 @@ class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         if(root is None):
             return True
+        
+            
         else:
+            mylist = []
+            if( not root.left and not root.right):
+                return True
+            if( not root.left or not root.right):
+                return False
             if(root.left is not None and root.right is not None):
-                return self.checkSame(root.left, root.right)
-#             self.pRoots.append(root.left)
-#             self.qRoots.append(root.right)
-            
-#             for x,temp in enumerate(self.pRoots):
-#                 if(self.flag == 1):
-#                     break
-#                 p = self.pRoots[x]
-#                 q = self.qRoots[x]
-                
-                
-#                 while(self.flag == 0):
-#                     if(p.right is not None and q.left is not None ):
-#                             self.pRoots.append(p.right)
-#                             self.qRoots.append(q.right)
-#                     if(p.right is None):
-#                         self.flag = 1
-#                     if(q.left is None):
-#                         self.flag = 1
-#                     if(p.right is None and q.left is None):        
-#                         self.flag = 0
-#                     elif(self.flag == 1):
-#                         break        
-                
-                    
-#                     if(p.val != q.val):
-#                         self.flag = 1
-#                         break
-
-                
-                
-            
-
+                self.checkSame(root.left, root.right)
+                if not self.flag:
+                    if root.left and root.right:
+                        mylist.append(root.left)
+                        mylist.append(root.right)
+                        while(len(mylist)):
+                            p = mylist.pop()
+                            q = mylist.pop()
+                        
+                            if(p.val !=  q.val):
+                                self.flag = 1
+                                break
+                        
+                            if(p.left and q.right):
+                                mylist.append(p.left)
+                                mylist.append(q.right)
+                            elif(p.left or q.right):
+                                self.flag = 1
+                                break
+                            
+                            if(p.right and q.left):
+                                mylist.append(p.right)
+                                mylist.append(q.left)
+                            elif(p.right or q.left):
+                                self.flag = 1
+                                break
+                    elif root.left or root.right:
+                        self.flag =1 
+                        
             if(self.flag == 1):
                 return False
             return True
